55827e85fdaac79291fdf8b2ae12de70d4251a80
Piotr Gorczyca <piotr.gorczyca@tu-dresden.de>
2026-01-08 23:36:15 +0100

    Fix pyshacl.

--- validator.sh
+++ validator.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 data_graph_file="data_graph.ttl"
-pyshacl_location="kimeds_env/bin/pyshacl"
+python_bin="kimeds_env/bin/python"
 
 # input from stdin
 input=$(cat)
@@ -12,7 +12,7 @@ shapes_location="$1"
 echo "$input" > "$data_graph_file"
 
 # run the validator
-output="$($pyshacl_location -s $shapes_location $data_graph_file -f json-ld)" # "json-ld by" default. Other alternatives: human,turtle,xml,json-ld,nt,n3
+output="$($python_bin -m pyshacl -s $shapes_location $data_graph_file -f json-ld)" # "json-ld by" default. Other alternatives: human,turtle,xml,json-ld,nt,n3
 echo -e "$output"
 
 # remove temp. file
